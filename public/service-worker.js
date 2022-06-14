b0961e66f34c5eaafa6cdb06cafd02bcc67e2de6
Samantha Wolff <samanthawolff07@gmail.com>
2022-06-14 09:39:19 -0500

    service worker fetch requests

--- public/service-worker.js
+++ public/service-worker.js
@@ -74,5 +74,15 @@ self.addEventListener('fetch', function(evt) {
         return;
     }
 
-    
-})
+    evt.respondWith(
+        fetch(evt.request).catch(function() {
+            return caches.match(evt.request).then(function(response) {
+                if (response) {
+                    return response;
+                } else if (evt.request.headers.get('accept').includes('text/html')) {
+                    return caches.match('/');
+                }
+            });
+        })
+    );
+});
